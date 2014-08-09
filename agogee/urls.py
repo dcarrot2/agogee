a457ed82031f42adb09b662507095d2abae5ecff
Katie Cunningham <katie@Katies-MacBook-Air.local>
2014-08-08 23:51:33 -0700

    Home page now goes to spartanMain.html

--- agogee/urls.py
+++ agogee/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import patterns, url
+from django.views.generic.base import TemplateView
 from agogee import views
 
 urlpatterns = patterns("",
+	url(r'^$', TemplateView.as_view(template_name='agogee/spartanMain.html'), name='index'),
 	url(r'^register/$', views.register, name='register'),
 	url(r'^login/$', views.user_login, name='login'),
 	url(r'^logout/$', views.user_logout, name='logout'),)
