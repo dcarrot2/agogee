14850a0761fa3cc56d3b025e4e07543aa0361aec
Katie Cunningham <katie@Katies-MacBook-Air.local>
2014-08-09 11:35:45 -0700

    List of posts now on community page

--- hackfit/urls.py
+++ hackfit/urls.py
@@ -1,8 +1,6 @@
 from django.conf.urls import patterns, include, url
 from django.conf import settings
 
-from posts.views import PostListView
-
 from django.contrib import admin
 admin.autodiscover()
 
@@ -14,8 +12,6 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^comments/', include('django.contrib.comments.urls')),
     url(r'^agogee/', include('agogee.urls', namespace='agogee')),
-
-    url(r'^$', PostListView.as_view(), name='home'),
 )
 
 if settings.DEBUG:
